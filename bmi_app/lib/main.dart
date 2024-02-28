7ef55d6ff731b0edd38f135a3e94305c17a80690
admin <rohitvarshney87@gmail.com>
2024-02-28 18:23:18 +0530

    completing the app

--- bmi_app/lib/main.dart
+++ bmi_app/lib/main.dart
@@ -1,11 +1,7 @@
-
-import 'dart:math';
-
 import 'package:bmi_app/result_page.dart';
 import 'package:flutter/material.dart';
 
 import 'drop_down.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +21,6 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -37,30 +32,49 @@ class _MyAppState extends State<MyApp> {
 class MyHomePage extends StatefulWidget {
   MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+double? calculateBMI(String selected_weight_val, String selected_height_val,
+    int weight, int heightCM, int heightIN) {
+  double? heightM;
+  double? weightKG;
 
-void calculate(){
+  if (selected_height_val == "Cm.") {
+    heightM = heightCM * 0.01;
+  } else {
+    heightIN += heightCM * 12;
+    heightM = heightIN * 0.0254;
+  }
+
+  if (selected_weight_val == "Lbs.") {
+    weightKG = weight * 0.453592;
+  } else {
+    weightKG = weight.toDouble();
+  }
+
+  if (heightM != null && weightKG != null) {
+    double result = weightKG / (heightM * heightM);
+    return result;
+  } else {
+    return null;
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
   String imagePath = "assets/images/men.png";
-  String selected_weight_val = "";
-  String selected_height_val = "";
+  String selected_weight_val = "Lbs.";
+  String selected_height_val = "Cm.";
   final List<String> heightOptions = ['Cm.', 'Ft.'];
   final List<String> weightOptions = ['Lbs.', 'Kg.'];
   TextEditingController ageController = TextEditingController();
   TextEditingController weightController = TextEditingController();
   TextEditingController heightControllerCM = TextEditingController();
   TextEditingController heightControllerIN = TextEditingController();
-
-
 
   Widget _buildIconButton(IconData iconData, int index) {
     return GestureDetector(
@@ -74,12 +88,12 @@ class _MyHomePageState extends State<MyHomePage> {
         decoration: BoxDecoration(
           boxShadow: _selectedIndex == index
               ? [
-            BoxShadow(
-              color: Colors.blueAccent.withOpacity(0.3),
-              blurRadius: 5,
-              spreadRadius: 1,
-            ),
-          ]
+                  BoxShadow(
+                    color: Colors.blueAccent.withOpacity(0.3),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  ),
+                ]
               : [],
         ),
         child: IconButton(
@@ -97,30 +111,32 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       body: SingleChildScrollView(
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            SizedBox(height: 35,),
-             Row(
+            SizedBox(
+              height: 35,
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Column(
                   children: [
-                    Text('BMI',
+                    Text(
+                      'BMI',
                       style: TextStyle(
                         fontSize: 45,
                         fontWeight: FontWeight.bold,
                         color: Colors.blue,
                       ),
                     ),
-                    Text('Calculator',
+                    Text(
+                      'Calculator',
                       style: TextStyle(
                         fontSize: 35,
                         fontWeight: FontWeight.bold,
@@ -132,35 +148,40 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 80,
                   width: 80,
                   child: ClipOval(
-                    child: _selectedIndex == 0 ? Image.asset("assets/images/profile_logo.png") : Image.asset("assets/images/profile_logo2.png"),
+                    child: _selectedIndex == 0
+                        ? Image.asset("assets/images/profile_logo.png")
+                        : Image.asset("assets/images/profile_logo2.png"),
                     clipBehavior: Clip.hardEdge,
-
                   ),
                 )
               ],
             ),
-            SizedBox(height: 25,),
-
+            SizedBox(
+              height: 25,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Container(height: 600,
-                width: 250,
+                Container(
+                  height: 600,
+                  width: 250,
                   decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: _selectedIndex == 0? AssetImage("assets/images/men.png") : AssetImage("assets/images/women.png"),
-                      fit:BoxFit.fitHeight,
-                    )
-                  ),
+                      image: DecorationImage(
+                    image: _selectedIndex == 0
+                        ? AssetImage("assets/images/men.png")
+                        : AssetImage("assets/images/women.png"),
+                    fit: BoxFit.fitHeight,
+                  )),
                 ),
-
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Gender",style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.blue,
+                    Text(
+                      "Gender",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.blue,
                       ),
                     ),
                     Row(
@@ -170,64 +191,73 @@ class _MyHomePageState extends State<MyHomePage> {
                         _buildIconButton(Icons.female, 1),
                       ],
                     ),
-                    Text("Age",style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.blue,
-                    ),),
-
+                    Text(
+                      "Age",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.blue,
+                      ),
+                    ),
                     Container(
                       margin: EdgeInsets.only(top: 10),
                       // height: , // Set the desired height
-                      width:  120, // Set the desired width
+                      width: 120, // Set the desired width
                       child: TextField(
                         controller: ageController,
                         keyboardType: TextInputType.number,
                         decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Age"
-                        ),
+                            border: OutlineInputBorder(), labelText: "Age"),
                       ),
                     ),
-
                     Row(
                       children: [
-                        Text("Height",style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.blue,
+                        Text(
+                          "Height",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.blue,
+                          ),
                         ),
+                        SizedBox(
+                          width: 25,
                         ),
-
-                        SizedBox(width: 25,),
                         DropdownButtonExample(
                           list: heightOptions,
-                          onChanged: (String value){
+                          onChanged: (String value) {
                             setState(() {
-                              selected_weight_val = value;
+                              selected_height_val = value;
                             });
                           },
                         ),
-
                       ],
                     ),
-                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: [
                         Container(
-                          width: selected_weight_val == "Ft." ? 60 :120, // Set the desired width
+                          width: selected_height_val == "Ft."
+                              ? 60
+                              : 120, // Set the desired width
                           child: TextField(
                             controller: heightControllerCM,
                             keyboardType: TextInputType.number,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(),
-                              labelText: selected_weight_val == "Ft." ? "Ft." :"CM.",
+                              labelText:
+                                  selected_height_val == "Ft." ? "Ft." : "CM.",
                             ),
                           ),
                         ),
-                        SizedBox(width: 10,),
+                        SizedBox(
+                          width: 10,
+                        ),
                         Visibility(
-                          visible: selected_weight_val == 'Ft.', // Set visibility based on selected value
+                          visible: selected_height_val ==
+                              'Ft.', // Set visibility based on selected value
                           child: Container(
                             // margin: EdgeInsets.only(left: 10),
                             width: 60,
@@ -243,29 +273,32 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ],
                     ),
-
                     Row(
                       children: [
-                        Text("Weight",style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.blue,
+                        Text(
+                          "Weight",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.blue,
+                          ),
                         ),
+                        SizedBox(
+                          width: 25,
                         ),
-
-                        SizedBox(width: 25,),
                         DropdownButtonExample(
                           list: weightOptions,
-                          onChanged: (String value){
+                          onChanged: (String value) {
                             setState(() {
-                              selected_height_val = value;
+                              selected_weight_val = value;
                             });
                           },
                         ),
-
                       ],
                     ),
-                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: [
                         Container(
@@ -275,41 +308,49 @@ class _MyHomePageState extends State<MyHomePage> {
                             keyboardType: TextInputType.number,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(),
-                              labelText: selected_height_val == "Kg." ? "Kg." :"Lbs.",
+                              labelText:
+                                  selected_weight_val == "Kg." ? "Kg." : "Lbs.",
                             ),
                           ),
                         ),
                       ],
                     ),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                   ],
                 ),
               ],
             ),
-            SizedBox(height: 35,),
-
-
-
-
+            SizedBox(
+              height: 35,
+            ),
             GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ResultPage()));
+              onTap: () {
+                int weight = int.tryParse(weightController.text) ?? 0;
+                int heightCM = int.tryParse(heightControllerCM.text) ?? 0;
+                int heightIN = int.tryParse(heightControllerIN.text) ?? 0;
+
+                double? bmiResult = calculateBMI(selected_weight_val,
+                    selected_height_val, weight, heightCM, heightIN);
+
+                if (!bmiResult!.isNaN) {
+                  print("BMI: $bmiResult");
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ResultPage(
+                              ageController.text.toString(),
+                              _selectedIndex == 0 ? "Male" : "Female",
+                              heightControllerCM.text.toString(),
+                              heightControllerIN.text.toString(),
+                              weightController.text.toString(),
+                              bmiResult,
+                              selected_height_val.toString(),
+                              selected_weight_val.toString(),
+                          )));
+                } else {
+                  print("Invalid input");
+                }
               },
               child: Container(
-
                 height: 50,
                 width: 200,
                 decoration: BoxDecoration(
@@ -332,9 +373,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-
-
-        ],
+          ],
         ),
       ),
     );
--- bmi_app/lib/result_page.dart
+++ bmi_app/lib/result_page.dart
@@ -1,13 +1,66 @@
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatefulWidget {
-  const ResultPage({super.key});
+  final String Age;
+  final String Gender;
+  final String heightCm,
+      heightIn,
+      weight;
+  final double result;
+  final String selected_weight_val,
+      selected_height_val;
+  const ResultPage(
+      this.Age,
+      this.Gender,
+      this.heightCm,
+      this.heightIn,
+      this.weight,
+      this.result,
+      this.selected_height_val,
+      this.selected_weight_val,
+      {Key? key})
+      : super(key: key);
 
   @override
   State<ResultPage> createState() => _ResultPageState();
 }
 
 class _ResultPageState extends State<ResultPage> {
+  String resultString = "";
+
+  @override
+  void initState() {
+    super.initState();
+    FindResultString();
+  }
+
+  void FindResultString() {
+    if (widget.result <= 18.5) {
+      resultString = "UnderWeight";
+    } else if (widget.result > 18.5 && widget.result < 25) {
+      resultString = "Normal";
+    } else if (widget.result >= 25 && widget.result <= 30) {
+      resultString = "OverWeight";
+    } else if (widget.result > 30) {
+      resultString = "Obesity";
+    }
+  }
+
+  Color getColorForResultString(String resultString) {
+    switch (resultString) {
+      case "UnderWeight":
+        return Colors.blue; // Change color to your preference
+      case "Normal":
+        return Colors.green; // Change color to your preference
+      case "OverWeight":
+        return Colors.orange; // Change color to your preference
+      case "Obesity":
+        return Colors.red; // Change color to your preference
+      default:
+        return Colors.black; // Default color if resultString doesn't match any case
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,117 +68,129 @@ class _ResultPageState extends State<ResultPage> {
         title: Text("Calculation Result"),
         centerTitle: true,
       ),
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("BMI Score",style: TextStyle(
-              fontSize: 25,
-            fontWeight: FontWeight.w500
-            ),),
-          Text("23,6",style: TextStyle(
-              fontSize: 65,
-            fontWeight: FontWeight.bold,
-            color: Colors.green
-            ),),
-          Text("Normal",style: TextStyle(
-              fontSize: 35,
-            fontWeight: FontWeight.bold,
-            color: Colors.green
-            ),),
-          SizedBox(height: 45,),
+          Text(
+            "BMI Score",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+          ),
+          Text(
+            widget.result.toStringAsFixed(2),
+            style: TextStyle(
+                fontSize: 65, fontWeight: FontWeight.bold, color: Colors.green),
+          ),
+          Text(
+            resultString,
+            style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: getColorForResultString(resultString)),
+          ),
+          SizedBox(
+            height: 45,
+          ),
           Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-
                 children: [
-                  Text('Gender:',
-                    style:TextStyle(color: Colors.blue, fontSize: 18) ,),
+                  Text(
+                    'Gender:',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  ),
                   SizedBox(width: 8),
-                 Text("M"),
+                  Text("${widget.Gender}"),
                 ],
               ),
               SizedBox(height: 8),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-
                 children: [
-                  Text('Age:',style:TextStyle(color: Colors.blue, fontSize: 18) ,),
+                  Text(
+                    'Age:',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  ),
                   SizedBox(width: 8),
-                  Text("32"),
+                  Text("${widget.Age}"),
                 ],
               ),
               SizedBox(height: 8),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-
                 children: [
-                  Text('Height:',style:TextStyle(color: Colors.blue, fontSize: 18) ,),
+                  Text(
+                    'Height:',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  ),
                   SizedBox(width: 8),
-                  Text("5'2"),
+                  Text(widget.selected_height_val != "Cm."
+                      ? "${widget.heightCm} Ft.' ${widget.heightIn} Inch"
+                      : "${int.parse(widget.heightCm) * 0.01} Mtr. '"),
                 ],
               ),
               SizedBox(height: 8),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-
                 children: [
-                  Text('Weight:',style:TextStyle(color: Colors.blue, fontSize: 18) ,),
+                  Text(
+                    'Weight:',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  ),
                   SizedBox(width: 8),
-                  Text("85"),
+                  Text(widget.selected_weight_val != "Lbs."
+                      ? "${widget.weight} Kg"
+                      : "${widget.weight} Lbs"),
                 ],
               ),
               SizedBox(height: 8),
             ],
           ),
-
           Padding(
-            padding: const EdgeInsets.only(left: 25.0,right: 25,top: 10,bottom: 10),
+            padding: const EdgeInsets.only(
+                left: 25.0, right: 25, top: 10, bottom: 10),
             child: Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.yellow.shade800, width: 2),
-                gradient: LinearGradient(
-                  colors: [Colors.yellow.shade100, Colors.yellow.shade400],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.yellow.shade800, width: 2),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.yellow.shade100,
+                      Colors.yellow.shade400
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
                 ),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Heading',
-                    style: TextStyle(
-                      color: Colors.yellow.shade800,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Heading',
+                      style: TextStyle(
+                        color: Colors.yellow.shade800,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Text(
-                    'Lorem ipsum dolor sit amet consectetur adipiscing el',
-                    style: TextStyle(
-                      color: Colors.yellow.shade800,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400
+                    Text(
+                      'Lorem ipsum dolor sit amet consectetur adipiscing el',
+                      style: TextStyle(
+                          color: Colors.yellow.shade800,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400),
                     ),
-                  ),
-                ],
-              )
-            ),
+                  ],
+                )),
           )
-
         ],
       ),
-
-
     );
   }
 }
